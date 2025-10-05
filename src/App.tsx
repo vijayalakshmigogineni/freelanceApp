35ffb0b8ab4e203be040261ddbcaa58d65c76795
vijayalakshmigogineni <vijayalakshmigogineni2005@gmail.com>
2025-10-05 19:31:34 +0530

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,39 @@
+import { Toaster } from "@/components/ui/toaster";
+import { Toaster as Sonner } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import Auth from "./pages/Auth";
+import Home from "./pages/Home";
+import PostTask from "./pages/PostTask";
+import Emergency from "./pages/Emergency";
+import Profile from "./pages/Profile";
+import TaskDetails from "./pages/TaskDetails";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const App = () => (
+  <QueryClientProvider client={queryClient}>
+    <TooltipProvider>
+      <Toaster />
+      <Sonner />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/post-task" element={<PostTask />} />
+          <Route path="/emergency" element={<Emergency />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/task/:id" element={<TaskDetails />} />
+          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+      </BrowserRouter>
+    </TooltipProvider>
+  </QueryClientProvider>
+);
+
+export default App;
